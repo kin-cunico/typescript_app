5981cab4718eafd5990c27f4cbe4cc61c33e8ca7
kin-cunico <124842064+kin-cunico@users.noreply.github.com>
2023-07-20 21:52:06 +0100

    with components to main elements in the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center">
-			<div className="rounded-sm flex flex-col align-middle justify-around bg-gray-700 w-96 h-40 items-center">
-				<h1 className="text-yellow-500 text-xl">Under construction</h1>
-				<p className="text-red-400">contact@kincunico.dev</p>
-			</div>
+			<Navbar />
+			<Hero />
+			<Footer />
 		</main>
 	);
 }
